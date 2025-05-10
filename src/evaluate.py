20df5520ef82aae5adad111aaabd2f0b514ac108
Hyojun Park <qkrgywns1028@gmail.com>
2025-05-10 16:16:37 +0900

    evaluate.py: 다중 시각화

--- src/evaluate.py
+++ src/evaluate.py
@@ -5,9 +5,9 @@ def _metric(pred, tgt, thr):
     dice=(2*inter+1e-6)/(p.sum()+tgt.sum()+1e-6); iou=(inter+1e-6)/(union+1e-6)
     return dice.item(), iou.item()
 
-def _vis(img, m, p):
+def _vis(img, m, p, thr):
     import numpy as np
-    img=img.squeeze().cpu().numpy(); m=m.squeeze().cpu().numpy(); p=(p.squeeze().cpu().numpy()>0.3)
+    img=img.squeeze().cpu().numpy(); m=m.squeeze().cpu().numpy(); p=(p.squeeze().cpu().numpy()>thr)
     fig,ax=plt.subplots(1,3,figsize=(12,4))
     for a,d,t in zip(ax,[img,m,p],["Img","GT","Pred"]):
         a.imshow(d,cmap='gray'); a.axis('off'); a.set_title(t)
@@ -19,5 +19,8 @@ def evaluate(model, loader, device, thr=0.5, vis=False):
         for k,(x,y) in enumerate(loader):
             x,y=x.to(device),y.to(device); p=torch.sigmoid(model(x))
             di,io=_metric(p,y,thr); d+=di; i+=io
-            if vis and k==0: _vis(x[0],y[0],p[0])
+            if vis and k==0: 
+                for i in range(10):
+                    _vis(x[i],y[i],p[i], thr)
+                    plt.pause(0.1)
     return d/n,i/n
