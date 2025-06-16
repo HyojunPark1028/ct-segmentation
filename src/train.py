26346df5de031609f2c492c6ff30b8ed0d63ca93
Hyojun Park <qkrgywns1028@gmail.com>
2025-06-16 22:12:59 +0900

    test

--- src/train.py
+++ src/train.py
@@ -181,8 +181,8 @@ def main(cfg):
             train_loss = 0
             train_start = time.time()
             
-            step_count = 0            
-            epoch_start_time = time.time()
+            # step_count = 0            
+            # epoch_start_time = time.time()
 
             # 훈련 루프
             loop = tqdm(train_dl, desc=f"Fold {fold+1} Epoch {epoch}/{cfg.train.epochs}", leave=False)
@@ -195,6 +195,9 @@ def main(cfg):
                 with autocast(device_type='cuda'):
                     if isinstance(model, MedSAM):
                         preds, preds_iou_for_log = model(x) # MedSAM은 (마스크, IoU) 튜플 반환
+                        print(f"[MedSAM] Prediction mask shape: {pred.shape}")  # e.g., torch.Size([4, 1, 1024, 1024])
+                        print(f"[MedSAM] Ground truth mask shape: {y.shape}")  # e.g., torch.Size([4, 1, 1024, 1024])
+
                         loss = criterion(preds, y)
                     else:
                         preds = model(x) # output 이름은 preds로 통일
@@ -221,7 +224,7 @@ def main(cfg):
                 torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
                 scaler.step(optimizer)
                 scaler.update()
-                step_count += 1
+                # step_count += 1
                 train_loss += loss.item()
                 
                 # 훈련 중 현재 배치에 대한 평균 예측값 로깅
@@ -301,8 +304,8 @@ def main(cfg):
             gc.collect()
             torch.cuda.empty_cache()
         
-            epoch_time = time.time() - epoch_start_time
-            print(f"[Epoch {epoch+1}] Time: {epoch_time:.2f}s | Steps: {step_count}")       
+            # epoch_time = time.time() - epoch_start_time
+            # print(f"[Epoch {epoch+1}] Time: {epoch_time:.2f}s | Steps: {step_count}")       
 
         # --- Fold 학습 완료 후 처리 ---
         # 폴드별 에포크 메트릭 저장
